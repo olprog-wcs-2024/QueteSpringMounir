ca669dbcc567b2bdea0e45076e7077412fd274d1
gaouimounir <mounir.gaoui@outlook.com>
2024-07-01 16:24:18 +0200

    first commit

---
+++ src/main/java/Quete/spring/QueteJPA1/Wizard.java
@@ -0,0 +1,33 @@
+package Quete.spring.QueteJPA1;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Wizard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private Date birthday;
+    private String birthPlace;
+    private String biography;
+    private boolean muggle;
+
+    public Wizard(Long id, String firstName, String lastName, Date birthday, String birthPlace, String biography, boolean muggle) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
+        this.birthPlace = birthPlace;
+        this.biography = biography;
+        this.muggle = muggle;
+    }
+}
